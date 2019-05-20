b76983cca2691915817e5e765ed68fc56517dd98
albertocruzdg <cruz.alberto92@gmail.com>
2019-05-20 00:15:17 -0300

    Correcting let

--- bubble-sort.js
+++ bubble-sort.js
@@ -2,13 +2,13 @@ var list = [54,2,13,55,3,256,1,34234,5];
 
 console.log("Original: \t" + list);
 
-for (i = 0; i < list.length; i++) {
+for (let i = 0; i < list.length; i++) {
 
-    for (j = 0; j < list.length - i; j++) {
+    for (let j = 0; j < list.length - i; j++) {
         
         if (list[j] > list[j + 1]) {
             
-            var aux = list[j];
+            let aux = list[j];
             
             list[j] = list[j + 1];
             
